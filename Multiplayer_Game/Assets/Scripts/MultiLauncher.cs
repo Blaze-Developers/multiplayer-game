ad95646bb01f1276ad47263c972302db9e22ec48
Your Name <70378969+KAIHATSU-dev@users.noreply.github.com>
2021-07-29 20:52:23 +0530

    Working on Multiplayer

---
+++ Multiplayer_Game/Assets/Scripts/MultiLauncher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MultiLauncher : MonoBehaviourPunCallbacks
+{
+    void Start()
+	{
+		Debug.Log("Connecting to Master");
+		PhotonNetwork.ConnectUsingSettings();
+	}
+    public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connected to Master");
+		PhotonNetwork.JoinLobby();
+		
+	}
+    public override void OnJoinedLobby()
+	{
+		
+		Debug.Log("Joined Lobby");		
+	}
+	public override void OnJoinedRoom()
+	{
+		MenuManager.Instance.OpenMenu("room");
+		roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+		Player[] players = PhotonNetwork.PlayerList;
+
+		foreach (Transform child in playerListContent)
+		{
+			Destroy(child.gameObject);
+		}
+
+		for (int i = 0; i < players.Count(); i++)
+		{
+			Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+		}
+
+		startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+	}
+	public override void OnCreateRoomFailed(short returnCode, string message)
+	{
+		errorText.text = "Room Creation Failed: " + message;
+		Debug.LogError("Room Creation Failed: " + message);
+		MenuManager.Instance.OpenMenu("error");
+	}
+	public void CreateRoom()
+	{
+		if (string.IsNullOrEmpty(roomNameInputField.text))
+		{
+			return;
+		}
+		PhotonNetwork.CreateRoom(roomNameInputField.text);
+		MenuManager.Instance.OpenMenu("Loading");
+	}
+    
+    
+}
