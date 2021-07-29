0a7a24511f09ca3541f4541d975874deae02ee98
Your Name <70378969+KAIHATSU-dev@users.noreply.github.com>
2021-07-29 23:36:18 +0530

    URP STARTED

--- Multiplayer_Game/Assets/Scripts/MultiLauncher.cs
+++ Multiplayer_Game/Assets/Scripts/MultiLauncher.cs
@@ -24,37 +24,15 @@ public class MultiLauncher : MonoBehaviourPunCallbacks
 	public override void OnJoinedRoom()
 	{
 		MenuManager.Instance.OpenMenu("room");
-		roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+		
 
-		Player[] players = PhotonNetwork.PlayerList;
-
-		foreach (Transform child in playerListContent)
-		{
-			Destroy(child.gameObject);
-		}
-
-		for (int i = 0; i < players.Count(); i++)
-		{
-			Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
-		}
-
-		startGameButton.SetActive(PhotonNetwork.IsMasterClient);
 	}
 	public override void OnCreateRoomFailed(short returnCode, string message)
 	{
-		errorText.text = "Room Creation Failed: " + message;
-		Debug.LogError("Room Creation Failed: " + message);
+		
 		MenuManager.Instance.OpenMenu("error");
 	}
-	public void CreateRoom()
-	{
-		if (string.IsNullOrEmpty(roomNameInputField.text))
-		{
-			return;
-		}
-		PhotonNetwork.CreateRoom(roomNameInputField.text);
-		MenuManager.Instance.OpenMenu("Loading");
-	}
+	
     
     
 }
