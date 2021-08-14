71b71bdf18c1b1368266a1974b1e4433f4aa1a90
Your Name <70378969+KAIHATSU-dev@users.noreply.github.com>
2021-08-14 09:14:53 +0530

    New PlayerController

---
+++ Multiplayer_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+	
+	Rigidbody rb;
+    [SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+	float verticalLookRotation;
+	bool grounded;
+	Vector3 smoothMoveVelocity;
+	Vector3 moveAmount;
+
+	[SerializeField] GameObject cameraHolder;
+	PlayerManager playerManager;
+
+	PhotonView PV;
+
+	const float maxHealth = 100f;
+	float currentHealth = maxHealth;
+
+    private void Start()
+    {
+        if(!PV.IsMine)
+        {
+			Destroy(GetComponentInChildren<Camera>().gameObject);
+		}
+    }
+
+    void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		PV = GetComponent<PhotonView>();
+
+		playerManager = PhotonView.Find((int)PV.InstantiationData[0]).GetComponent<PlayerManager>();
+	}
+
+	public void SetGroundedState(bool _grounded)
+	{
+		grounded = _grounded;
+	}
+
+	void FixedUpdate()
+	{
+		if (!PV.IsMine)
+			return;
+
+		rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+	}
+
+	private void Update()
+    {
+		if (!PV.IsMine)
+			return;
+
+		Look();
+		Move();
+		Jump();
+	}
+
+    void Look()
+	{
+		transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mouseSensitivity);
+
+		verticalLookRotation += Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+		verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+		cameraHolder.transform.localEulerAngles = Vector3.left * verticalLookRotation;
+	}
+	void Move()
+	{
+		Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+		moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+	}
+
+	void Jump()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && grounded)
+		{
+			rb.AddForce(transform.up * jumpForce);
+		}
+	}
+
+
+}
