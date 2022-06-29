50e79e8dae0f792f60fffc8d2ed9e3283ea74fd5
Your Name <70378969+KAIHATSU-dev@users.noreply.github.com>
2022-06-29 14:15:58 +0530

    TPP in test phase

---
+++ Multiplayer_Game/Assets/Scripts/Third Person Controller.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class ThirdPersonController : MonoBehaviour
+{
+	Rigidbody rb;
+
+	[SerializeField] float mouseSensitivity, sprintSpeed, walkSpeed, jumpForce, smoothTime;
+
+	bool grounded;
+	Vector3 smoothMoveVelocity;
+	Vector3 moveAmount;
+
+	PlayerManager playerManager;
+
+	PhotonView PV;
+
+	private void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+		if (!PV.IsMine)
+		{
+			Destroy(GetComponentInChildren<Camera>().gameObject);
+			Destroy(rb);
+		}
+	}
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+		PV = GetComponent<PhotonView>();
+
+		playerManager = PhotonView.Find((int)PV.InstantiationData[0]).GetComponent<PlayerManager>();
+	}
+
+	public void SetGroundedState(bool _grounded)
+	{
+		grounded = _grounded;
+	}
+
+	void FixedUpdate()
+	{
+		if (!PV.IsMine)
+			return;
+
+		rb.MovePosition(rb.position + transform.TransformDirection(moveAmount) * Time.fixedDeltaTime);
+	}
+
+	private void Update()
+	{
+		if (!PV.IsMine)
+			return;
+
+		Move();
+		Jump();
+	}
+
+
+	public void Move()
+	{
+
+
+		Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+		moveAmount = Vector3.SmoothDamp(moveAmount, moveDir * (Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed), ref smoothMoveVelocity, smoothTime);
+
+	}
+
+	void Jump()
+	{
+		if (Input.GetKeyDown(KeyCode.Space) && grounded)
+		{
+			rb.AddForce(transform.up * jumpForce);
+		}
+	}
+
+
+}
